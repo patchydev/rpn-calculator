2f1b8f519ecc96e5bf10857cb569f0016f470c5f
Isabelle <patchydev@proton.me>
2025-03-18 20:20:05 -0400

    feat: push to stack, exit program

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import System.Exit (exitWith, ExitCode(..))
+
+repl :: [Integer] -> IO ()
+repl nums = do
+    input <- getLine
+    if input == "exit" then
+        exitWith(ExitSuccess)
+    else if input == "print" then do
+        print nums
+        repl nums
+    else do
+        let nums2 = (stringToInt input) : nums
+        repl nums2
+
+stringToInt :: String -> Integer
+stringToInt s = read s
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    let nums = []
+    repl nums
