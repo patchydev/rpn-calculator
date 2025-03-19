e82009d9ca0d76c94848e19935af0f00c3b59ee6
Isabelle <patchydev@proton.me>
2025-03-19 17:43:12 -0400

    refactor: use case instead of if/else

--- app/Main.hs
+++ app/Main.hs
@@ -6,34 +6,22 @@ import Text.Read (readMaybe)
 repl :: [Integer] -> IO ()
 repl nums = do
     input <- getLine
-    if input == "exit" then
-        exitWith(ExitSuccess)
-    else if input == "print" then do
-        print nums
-        repl nums
-    else if input == "ac" then do
-        repl []
-    else if input == "c" then do
-        if length nums > 0 then do
-            repl (init nums)
-        else do
+    case input of
+        "exit" -> exitWith(ExitSuccess)
+        "print" -> do
+            print nums
             repl nums
-    else if input == "+" then do
-        repl [sum nums]
-    else if input == "*" then do
-        repl [product nums]
-    else if input == "-" then do
-        repl [subList nums]
-    else if input == "/" then do
-        repl [divList nums]
-    else do
-        let inputN = stringToInt input
-        case inputN of
-            Nothing -> do
-                exitWith(ExitFailure 1)
-            Just n -> do
-                let nums2 = nums ++ [n]
-                repl nums2
+        "ac" -> repl []
+        "c" -> case nums of
+            [] -> repl []
+            _ -> repl (init nums)
+        "+" -> repl [sum nums]
+        "*" -> repl [product nums]
+        "-" -> repl [subList nums]
+        "/" -> repl [divList nums]
+        _ -> case stringToInt input of
+            Nothing -> exitWith (ExitFailure 1)
+            Just n -> repl (nums ++ [n])
 
 subList :: [Integer] -> Integer
 subList [] = 0
